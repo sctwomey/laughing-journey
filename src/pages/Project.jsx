22e30f4c0336cb4b825872da19c9729079038d76
sctwomey <sctwomey@gmail.com>
2023-11-13 11:58:48 -0800

    Modify project file

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -4,8 +4,10 @@ export default function Project({ projects }) {
     return (
         <div>
             <div className="project-description">
-                <a href={projects.pageUrl} className="project-title" target="_blank" rel="noreferrer">{projects.name}</a>
-                <a href={projects.githubUrl} target="_blank" rel="noreferrer"><span className="icon icon-github"></span></a>
+                <div className="row proj-urls">
+                    <a href={projects.pageUrl} className="project-title" target="_blank" rel="noreferrer">{projects.name}</a>
+                    <a href={projects.githubUrl} target="_blank" rel="noreferrer"><span className="icon icon-github"></span></a>
+                </div>
                 <p>{projects.description}</p>
             </div>
         </div>
