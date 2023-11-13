1d6c78dbe5ba9d04b4f0950ae3e629dd66d8118a
sctwomey <sctwomey@gmail.com>
2023-11-13 12:18:39 -0800

    Modified project file and index.css files

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -4,11 +4,13 @@ export default function Project({ projects }) {
     return (
         <div>
             <div className="project-description">
-                <div className="row proj-urls">
+                <div className="container d-flex justify-content-between">
                     <a href={projects.pageUrl} className="project-title" target="_blank" rel="noreferrer">{projects.name}</a>
                     <a href={projects.githubUrl} target="_blank" rel="noreferrer"><span className="icon icon-github"></span></a>
                 </div>
-                <p>{projects.description}</p>
+                <div className="proj-desc">
+                    <p>{projects.description}</p>
+                </div>
             </div>
         </div>
     );
